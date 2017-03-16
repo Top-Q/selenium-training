5c40729ca15685c0110b25df248c6f849f2ff9d1
BenMark <bm7382598@gmail.com>
2017-03-16 14:19:44 +0200

    merge conflicts

--- src/main/java/il/co/topq/tests/SubmitNewIssueTest.java
+++ src/main/java/il/co/topq/tests/SubmitNewIssueTest.java
@@ -47,6 +47,46 @@ public class SubmitNewIssueTest extends AbstractTestCase{
 		 		.chooseMileStoneFromListByNameSpan("MS1.0.0");
 		 		
 	}
+	@Test(dataProvider="newProJectParameters")
+	public void AddingNewProject(String username,String password,String projectName,String projectDescription,String columnName)
+	{
+		String repoNameToTest = "test";
+		
+		SignInPage signInPage = introPage.clickOnLogInLink();
+		HomePage homePage  = signInPage
+				.typeToEmailTb(username)
+				.typeToPasswordTb(password)
+				.clickOnCommitBtn();
+		CodePage repositoryPage = homePage
+				.repositoriesWidget()
+				.clickOnRepositoryLnk(repoNameToTest);
+		ProjectsPage projectsPage = repositoryPage.clickOnProjectsTab(repoNameToTest);
+		projectsPage
+			.clickOnNewProjectBtn()
+			.typeToNewProjectNameTb(projectName)
+			.typeToNewProjectBodyTb(projectDescription)
+			.clickOnSaveProjectBtn()
+			.clickOnAddNewColumnLink()
+			.typeToNewColumnNameTb(columnName)
+			.clickOnCreateNewColumnBtn();
+				
+				
+	}
+	@Test(dataProvider="parametersForIssuesToColumns")
+	public void AddingIssuesToColumn(String userName,String passWord,String projectName,String columnName)
+	{
+String repoNameToTest = "test";
+		
+		SignInPage signInPage = introPage.clickOnLogInLink();
+		HomePage homePage  = signInPage
+				.typeToEmailTb(userName)
+				.typeToPasswordTb(passWord)
+				.clickOnCommitBtn();
+		CodePage repositoryPage = homePage
+				.repositoriesWidget()
+				.clickOnRepositoryLnk(repoNameToTest);
+		ProjectsPage projectsPage = repositoryPage.clickOnProjectsTab(repoNameToTest);
+	}
 	
 	@Test(dataProvider = "parameters")
 	public void fromStartToCreationOfMilestone(String username, String password)
@@ -76,10 +116,19 @@ public class SubmitNewIssueTest extends AbstractTestCase{
 		
 	}
 	
+	@DataProvider(name = "parametersForIssuesToColumns")
+	public Object[][] createDataForAddingIssuesToColumns() {
+		return new Object[][] { { "sudo.chmod.a.x777@gmail.com", "sudogit777", "This is project title", "C1" } };
+	}
 	
 	@DataProvider(name = "parameters")
 	public Object[][] createData() {
 		return new Object[][] { { "sudo.chmod.a.x777@gmail.com", "sudogit777"} };
+	}
+	
+	@DataProvider(name = "newProJectParameters")
+	public Object[][] createNewData() {
+		return new Object[][] { { "sudo.chmod.a.x777@gmail.com", "sudogit777", "This is project title","This is project description","column columns" } };
 	}
 	
 	@DataProvider(name = "excelparameters")
